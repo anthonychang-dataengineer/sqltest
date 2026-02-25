0b8a85b414831a2abb420dda456f5a8d0fafba0e
Anthony Chang <anthony.chang.edison@gmail.com>
2026-02-25 13:20:25 -0500

    Changed so that the query selects all columns, with limit 10

--- query_test.py
+++ query_test.py
@@ -7,7 +7,7 @@ df = pd.read_csv(r'c:\Users\17322\Downloads\sqltest\circuits.csv')
 df.to_sql('circuits', conn, if_exists='replace', index=False) 
 
 cursor = conn.cursor() 
-cursor.execute("INSERT INTO circuits (circuitId, circuitRef, name, location, country, lat, lng, alt, url) VALUES (999, 'bob', 'Bob Jones', 'Antarctica City','Antarctica', 100, 100, 10, 'www.bobjones.com');") 
+cursor.execute("SELECT * FROM circuits LIMIT 10;") 
 print(cursor.fetchall()) 
 
 conn.close()
