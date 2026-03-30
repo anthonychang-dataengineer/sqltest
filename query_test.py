de4c331eb76fdbfb555e28a07ecf4b22486ef0e4
Anthony Chang <anthony.chang.edison@gmail.com>
2026-03-30 17:08:53 -0400

    Changed LIMIT 10 to LIMIT 11

--- query_test.py
+++ query_test.py
@@ -7,7 +7,7 @@ df = pd.read_csv(r'c:\Users\17322\Downloads\sqltest\circuits.csv')
 df.to_sql('circuits', conn, if_exists='replace', index=False) 
 
 cursor = conn.cursor() 
-cursor.execute("SELECT * FROM circuits LIMIT 10;") 
+cursor.execute("SELECT * FROM circuits LIMIT 11;") 
 print(cursor.fetchall()) 
 
 conn.close()
